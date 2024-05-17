b39444df1f1d32d63ff9adbb7d5fe4ffa12c983e
JasmeetSgaba404 <jasmeetgaba77@gmail.com>
2024-05-17 23:31:46 +0530

    second commit

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,28 @@
+import Form from "../../../ui/invoices/create-form";
+import Breadcrumbs from "../../../ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Create Invoices',
+  }
+
+export default async function Page() {
+    const customer = await fetchCustomers();
+
+    return(
+        <main>
+            <Breadcrumbs
+            breadcrumbs={[
+                { label: 'Invoices', href: '/dashboard/invoices' },
+                {
+                    label: 'Create Invoices',
+                    href: '/dashboard/invoices/create',
+                    active: true,
+                },
+            ]}
+            />
+            <Form customers={customer} />
+        </main>
+    );
+}
